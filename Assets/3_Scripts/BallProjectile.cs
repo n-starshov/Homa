644e7b6a14b2486ef701c67b5718f375f32ad079
n.starshov <n.starshov@corp.nekki.ru>
2023-09-11 20:26:32 +0200

    init commit

---
+++ Assets/3_Scripts/BallProjectile.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallProjectile : MonoBehaviour
+{
+    new Rigidbody rigidbody;
+    int ColorIndex = 0;
+    new Renderer renderer;
+    Material originalMaterial;
+
+    TowerTile target;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        renderer = GetComponent<Renderer>();
+        if (renderer)
+            originalMaterial = renderer.material;
+
+        TileColorManager.Instance.OnColorListChanged += ResetColor;
+    }
+
+    private void OnDestroy()
+    {
+        if (TileColorManager.Instance)
+            TileColorManager.Instance.OnColorListChanged -= ResetColor;
+    }
+
+    public void ResetColor()
+    {
+        SetColor(TileColorManager.Instance.GetColor(ColorIndex));
+    }
+
+    public void SetColor(Color color)
+    {
+        renderer.sharedMaterial = TileColorManager.GetSharedMaterial(originalMaterial, color);
+    }
+
+    public void SetColorIndex(int index)
+    {
+        ColorIndex = index;
+        SetColor(TileColorManager.Instance.GetColor(index));
+    }
+
+    public void SetVelocity(Vector3 velocity)
+    {
+        transform.parent = null;
+        rigidbody.isKinematic = false;
+        rigidbody.AddForce(velocity, ForceMode.VelocityChange);
+    }
+
+    public void SetTarget(TowerTile target)
+    {
+        this.target = target;
+        GetComponent<Collider>().isTrigger = target.ColorIndex == ColorIndex;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        TowerTile tile = other.GetComponentInParent<TowerTile>();
+        if (tile == target) {
+            Explode();
+            tile?.Explode(true);
+        }
+    }
+
+    public void Explode()
+    {
+        Destroy(this.gameObject);
+    }
+}
